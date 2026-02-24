fe5773c14d0dc15363029511045743764e7602ab
AhmadouDIAW <ahmadoudiawdiaw@gmail.com>
2026-02-24 23:24:20 +0100

    derniere commit API/PHP

--- sqlbdd.sql
+++ sqlbdd.sql
@@ -10,7 +10,7 @@ CREATE TABLE IF NOT EXISTS users (
     role ENUM('etudiant', 'admin', 'restaurateur') NOT NULL,
     mot_de_passe VARCHAR(255) NOT NULL,
     date_inscription TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    date_validation DATETIME NULL, 
+    date_validation TIMESTAMP NULL DEFAULT NULL, 
     INDEX(email)
 );
 
